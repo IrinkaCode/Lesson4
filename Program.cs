cb9802e3f4a86a038a4b3894c31fdd69fb4ce99a
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-18 20:03:51 +0200

    изменения в дз

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿//int Moscow = 13015126;
-//int Piter = 5598486;
-//int Difference=((Piter % 10000 / 1000) - (Moscow % 10000 / 1000));
+﻿int Moscow = 13015126;
+int Piter = 5598486;
 
-//Console.WriteLine($"В Москве меньше жителей на {Difference} тысячи");
+Console.WriteLine(Math.Abs(Piter - Moscow) / 1000);
 
-double Year = 260548.56895;
-double Month = Year / 12;
-double WithNds = (Year / 12) - (Year / 12) / 100 * 13;
+//double Year = 260548.56895;
+//double Month = Year / 12;
+//double WithNds = (Year / 12) - (Year / 12) / 100 * 13;
 
-Console.WriteLine($"Работник зарабатывает в месяц {Month:F2}");
-Console.WriteLine($"Работник зарабатывает в месяц с вычетом НДФЛ {WithNds:F2}");
+//Console.WriteLine($"Работник зарабатывает в месяц {Month:F2}");
+//Console.WriteLine($"Работник зарабатывает в месяц с вычетом НДФЛ {WithNds:F2}");
+
+
+
